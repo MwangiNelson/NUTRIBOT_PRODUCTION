aa4b4204e992fe1773fc0923c3256b120749cb46
MwangiNelson <nelsonmwangi197@gmail.com>
2024-01-08 13:28:36 +0300

    feat:cors-policy

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 import requests
 import os
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/generate-text', methods=['POST'])
 def generate_text():
@@ -18,5 +20,5 @@ def generate_text():
         return jsonify({"error": response.text}), response.status_code
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT not set
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
